58812f5e799822518e3328123fb7dfc21e070878
Fabrice Ndui <fnduister@gmail.com>
2022-01-15 00:36:05 -0500

    Delete seereye-app directory

--- seereye-app/seereye_app/lib/services/services_macro.dart
+++
@@ -1,9 +0,0 @@
-const String NAME = 'sensorName';
-const String INSTANCES = 'instances';
-const String NODES = 'nodes';
-const String ADRESS = 'addresses';
-
-const String PUBLIC_IP = 'publicIp';
-
-const String BUILDER = 'builder';
-const String SHOWROOM = 'showroom';
